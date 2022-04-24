c3603c5a42bf860d6f2fb4a81a62e9773fa7f68a
lfeiyang <1449113674@qq.com>
2022-04-24 23:34:16 +0800

    首页

--- src/main/java/com/sy/config/SpringBootStarter.java
+++ src/main/java/com/sy/config/SpringBootStarter.java
@@ -1,6 +1,9 @@
 package com.sy.config;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 启动类
@@ -8,7 +11,14 @@ import org.springframework.boot.SpringApplication;
  * @author lfeiyang
  * @since 2022-04-23 21:54
  */
+@RestController
+@EnableAutoConfiguration
 public class SpringBootStarter {
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(SpringConfig.class, args);
     }
