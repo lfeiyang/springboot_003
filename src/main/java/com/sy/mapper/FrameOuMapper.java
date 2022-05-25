a93b2dd39caa27324bbbc68c13760133cb47a0da
lfeiyang <1449113674@qq.com>
2022-05-25 21:39:47 +0800

    Maven Helper

--- src/main/java/com/sy/mapper/FrameOuMapper.java
+++ src/main/java/com/sy/mapper/FrameOuMapper.java
@@ -3,5 +3,11 @@ package com.sy.mapper;
 import com.sy.model.FrameOu;
 import tk.mybatis.mapper.common.Mapper;
 
+/**
+ * mapper接口
+ *
+ * @author lfeiyang
+ * @since 2022-04-25 20:25
+ */
 public interface FrameOuMapper extends Mapper<FrameOu> {
 }
